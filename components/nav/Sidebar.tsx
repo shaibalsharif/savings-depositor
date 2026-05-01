aedef28bdefb03ea13e1aa99074f2276a073069b
shaibal1 <shaibal.tiller@gmail.com>
2026-05-02 00:53:48 +0600

    Delete obsolete Sidebar.tsx to fix build

--- components/nav/Sidebar.tsx
+++
@@ -1,7 +0,0 @@
-import { isManager } from "@/lib/auth";
-import { SidebarClient } from "./SidebarClient";
-
-export async function Sidebar() {
-  const managerRole = await isManager();
-  return <SidebarClient isManagerRole={managerRole} />;
-}
