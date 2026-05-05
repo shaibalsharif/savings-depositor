971eb2e7e7a3289aa7f94f341525708106b86fc3
shaibal1 <shaibal.tiller@gmail.com>
2026-05-05 18:02:05 +0600

    config: allow remote image patterns for uploadthing

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uploadthing.com',
+      },
+    ],
+  },
   webpack(config, { isServer }) {
     if (!isServer) {
       config.resolve.fallback = {
